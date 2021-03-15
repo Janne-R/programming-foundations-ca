e8168c4808b5b4d5ac88b642b3ff283bb97c4c9f
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-03-15 09:16:06 +0100

    Update script.js
    question 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// question 1
+var outOfStock = true;
+
+if (outOfStock === true) { 
+  console.log("Out of stock"); 
+} else { 
+   console.log("In stock"); 
+} 
+
+// question 2
+
+// question 3
+// question 4
+// question 5
+// question 6
+// question 7
