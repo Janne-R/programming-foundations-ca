c3ee8ba732a08b56b01e60b76f8f1e4c2c680c5c
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-03-15 11:56:32 +0100

    question 2 + 3

--- js/script.js
+++ js/script.js
@@ -8,8 +8,46 @@ if (outOfStock === true) {
 } 
 
 // question 2
+for (var i = 15; i <= 25; i++) { 
+  if (i === 17 || i === 20) {
+    console.log(i);
+  }
+} 
 
 // question 3
+var games = [
+	{
+		title: "Grand Theft Auto",
+		rating: 4.48,
+	},
+	{
+		title: "Portal 2",
+		rating: 3.5,
+	},
+	{
+		title: "Team Fortress",
+		rating: null,
+	},
+	{
+		title: "The Witcher",
+		rating: 3.0,
+	},
+	{
+		title: "The Elder Scrolls",
+		rating: 2.9,
+	},
+];
+
+var ul = document.querySelector("ul");
+
+for (var i = 0; i < games.length; i++) {
+  var rating = games[i].rating;
+  
+  if (rating !== null && rating < 3.5) {
+    ul.innerHTML = ul.innerHTML + "<li>" + games[i].title + ": " + games[i].rating + "</li>";
+  }
+}
+
 // question 4
 // question 5
 // question 6
